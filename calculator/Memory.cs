0ea41d789506575d5400bc90fd2bd9771223882b
Matsumoto Keita <matsukei88@gmail.com>
2019-10-14 21:26:43 +0900

    メモリクラス少し書き足し

--- calculator/Memory.cs
+++ calculator/Memory.cs
@@ -8,30 +8,40 @@ namespace calculator
 {
     class Memory
     {
-        private String[] memory = new String[4];
+        private String[] memory = new String[4];//フォームに反映するためゲッタ必要かも----------------------------
 
+        //コンストラクタ　nullでメモリを初期化
         public Memory()
         {
             int i;
             for (i = 0; i < 4; i++)
-                memory[i] = "0";
+                this.memory[i] = null;
         }
 
-        public void memory_save(int number,String data)
+        //指定番号のメモリにデータを格納
+        void memory_save(int number, String data)
         {
+            this.memory[number] = data;
         }
-        public String memory_read(int number) {
-            return "0";
-        }
-        public void memory_delete(int number)
+
+        //指定番号のメモリのデータを返す
+        String memory_read(int number)
         {
-            
+            return this.memory[number];
         }
-        public bool memory_check(int number)
+
+        //指定番号のメモリのデータを削除
+        void memory_delete(int number)
+        {
+            this.memory[number] = null;
+        }
+
+        //指定番号のメモリにデータがあるか
+        bool memory_check(int number)//どこで?-----------------------------------
         {
             return this.memory[number] == null;
         }
 
     }
-    
+
 }
